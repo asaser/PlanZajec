b92e825f08ddfca1dbb4e9427eaeaf43508a1303
Małgorzata Graja <graja.gosia@wp.pl>
2017-05-17 21:46:26 +0200

    Counting stored data in database and reading all id numbers

--- ModelAllSchedules.php
+++ ModelAllSchedules.php
@@ -11,7 +11,10 @@
 		<h1>Mój plan zajęć</h1>
 		<p id="paragraf"></p>
 		<button id="przycisk">Wyswietl dane</button>
-		<button onclick="saveData()">Zapisz dane</button>
+		<button onclick="saveData()">Zapisz dane do bazy</button>
+		<button onclick="countSchedules()">Zlicz dane w bazie</button>
+		<button onclick="printData()">Odtwórz numery id w konsoli</button>
+		
 
 		<!--Parsing one xml file to json-->
 		<?php
@@ -20,14 +23,7 @@
 	        $fileContents = trim(str_replace('"', "'", $fileContents));
 	        $simpleXml = simplexml_load_string($fileContents);
 	        $json = json_encode($simpleXml);
-	        //$json = str_replace(array(","), ",\n", $json);
 	        echo $json;
-
-
-			/*$xml = simplexml_load_string("plan.xml");
-			$json = json_encode($xml);
-			$array = json_decode($json,TRUE);
-			echo array;*/
 		?>
 		
 		
@@ -38,14 +34,13 @@
 			var data =<?php echo $json; ?>;
 			// initializing database
 			var ref = new Firebase("https://projekt1-4d649.firebaseio.com/web/saving-data/fireblog");
+			var usersRef = ref.child("all");
 			// saving data to database
 			function saveData(){
-				var usersRef = ref.child("all");
 				usersRef.set(data);
 			}
 
-
-			// PRINTING DATA
+			// SAVING IN LOCAL STORAGE AND PRINTING ON SCRREEN
 			// compresing string before storing in localStorage    
 			localStorage.setItem('dataString', JSON.stringify(data));
 			// decompresing item in localStorage
@@ -56,7 +51,35 @@
 					$("#paragraf").text(string);
 				});
 			});
-						
+
+			// COUNTING DATA IN DATABASE
+			//counting rows in order to know how many schedules and ids are stored (id numbers are needed to access all schedules)
+			var countRows = 0;
+			function countSchedules() {
+				ref.child("all/zasob/").on("child_added", function(snap) {
+				  countRows++;
+				  console.log("added", snap.key());
+				});
+				// length will always equal count, since snap.val() will include every child_added event
+				ref.child("all/zasob/").once("value", function(snap) {
+				  console.log("initial data loaded!", Object.keys(snap.val()).length === countRows);
+				  console.log("rows number", countRows);
+				  console.log("Snap length", Object.keys(snap.val()).length)
+				});
+			}
+			
+			// PRINTING DATA FROM DATABASE
+			function printData() {
+				// Attach an asynchronous callback to read the data at our posts reference
+				for (i = 0; i < countRows; i++) {
+					ref.child("all/zasob/" + i + "/@attributes/id").on("value", function(snapshot) {
+					  console.log(snapshot.val());
+					}, function (errorObject) {
+					  console.log("The read failed: " + errorObject.code);
+					});
+				}
+			}
+			
 		</script>
 	</body>
 </html>
