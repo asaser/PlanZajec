eca4743af32a212e0cfd9bf42ce23c6bd8172d01
Bartek Korytkowski <asaser11@gmail.com>
2017-05-20 18:26:00 +0200

    view

--- view.js
+++ view.js
@@ -1,25 +1,22 @@
 var view = {
 		display: function (data) {
-									// create new data view to be displayed
-			var elem, text, i;
+			// create new data view to be displayed
+			var elem, text;
 			this.clear();
-			for (i = 0; i < data.length; i++) {
-				elem = document.createElement("h3");
-				text = document.createTextNode(
-						data[i].code + "/" + data[i].name + " " + data[i].rate
-						);
-				elem.appendChild(text);
-				document.getElementById('dataview').appendChild(elem);
+			elem = document.createElement("h3");
+			text = document.createTextNode(data);
+			elem.appendChild(text);
+			document.getElementById('resultsView').appendChild(elem);
+			
+		},
+		clear: function () {
+			// remove old data
+			var dataView = document.getElementById('resultsView');
+			while (dataView.firstChild) {
+				dataView.removeChild(dataView.firstChild);
 			}
 		},
-		
-		
-		addCurrency: function () {
-			var currency = {
-					code: document.getElementById('code').value,
-					rate: document.getElementById('rate').value,
-					name: document.getElementById('name').value
-			}
-			controller.storeCurrency(currency);
+		find: function (data) {
+			controller.updateView(data);
 		}
 	};
